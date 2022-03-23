0ee3bdac5505317acbeb73214e4cf4b7ddccf7b1
waltiu <waltiu@163.com>
2022-03-23 22:31:02 +0800

    add: 样式优化

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,109 @@
-<script setup >
-import { watchEffect, reactive } from "vue";
-import { xLength, yLength } from "./components/constant";
-let blocks=[]
+<script setup>
+import { watchEffect, ref,isRef,reactive  } from "vue";
+import {
+  X_Length,
+  Y_Length,
+  MINE_RATE,
+  getSiblingBlock,
+} from "./components/constant";
+let blocks = ref([]);
+console.log(blocks,'block')
 const initBlocks = () => {
-   blocks = Array.from({ length: xLength }, (_,x) =>
-    Array.from({ length: yLength },(_,y)=>({
+  blocks.value = Array.from({ length: X_Length }, (_, x) =>
+    Array.from({ length: Y_Length }, (_, y) => ({
       y,
       x,
-      value:0
+      value: 0,
+      isBomb: false, 
+      isCovered:true
     }))
   );
-  console.log(blocks,'blocks');
+  console.log(blocks, "blocks");
 };
 
+const generateMines = () => {
+  blocks.value  = blocks.value .map((raw) => {
+    return raw.map((block) => {
+      return {
+        ...block,
+        isBomb: Math.random() < MINE_RATE,
+      };
+    });
+  });
+};
+
+const generateNumber = () => {
+  blocks.value  = blocks.value .map((raw) => {
+    return raw.map((block) => {
+      let value = 0;
+      if (!block.isBomb) {
+        getSiblingBlock(blocks.value, block).forEach((item) => {
+          if (item.isBomb) {
+            value = value  + 1;
+          }
+        });
+      }
+      return {
+        ...block,
+        value,
+      };
+    });
+  });
+};
+
+const getBlockStyles = (block) => {
+  if(!block.isCovered){
+    return {
+          backgroundColor: "gray",
+    }
+  }
+  return {
+    backgroundColor: "gray",
+    color:"gray"
+  };
+};
+
+ const clickBlock =(block)=>{
+  if(block.isBomb){
+    alert('you has been die ! ')
+  }
+  blocks.value [block.x][block.y].isCovered=false
+}
+
+console.log(isRef(blocks))
 initBlocks();
-console.log(blocks,'blocks')
+generateMines();
+generateNumber();
+console.log(blocks, "blocks");
 </script>
 
 <template>
   <div class="mines-sweeper">
-    <div v-for="raw of blocks " class="raw">
-        <span v-for="block of raw" class="block">
-        {{block.x}}:{{block.y}}
-        </span>
+    <div v-for="raw of blocks" class="raw">
+      <div v-for="block of raw" class="block" :style="getBlockStyles(block)" @click="clickBlock(block)">
+        <div v-if="block.isBomb">炸弹</div>
+        <div v-else>{{ block.isCovered }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 .mines-sweeper {
-  .raw{
+  .raw {
     margin: 12px;
+    display: flex;
+    align-items: center;
   }
-  .block{
+  .block {
+    cursor: pointer;
     margin: 8px;
+    padding: 2px;
+    border-radius: 2px;
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    text-align: center;
   }
 }
-
 </style>
--- src/components/constant.js
+++ src/components/constant.js
@@ -1,3 +1,29 @@
 
-export const  xLength=5
-export const  yLength=5
+export const  X_Length=5
+export const  Y_Length=5
+
+export const  MINE_RATE=0.4  // 炸弹的概率
+
+export const SIBLING_POSITIONS=[
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+]
+
+const isCorrectPosition =(x,y)=>{
+    return x>=0&&x<X_Length&&y<Y_Length&&y>=0
+}
+
+export const getSiblingBlock =(blocks,block)=>{
+   return  SIBLING_POSITIONS.map(([x,y])=>{
+        const x1=block.x+x
+        const y1=block.y+y
+        return isCorrectPosition(x1,y1)? blocks[x1][y1]:null
+    }).filter(Boolean)
+}
+
